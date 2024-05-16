ddc3e57878a484e5e7d0717a64c5c57eade2972b
rahul12km <bestrahul123r@gmail.com>
2024-05-16 09:19:23 +0530

    update

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -18,15 +18,15 @@ const Education = () => {
           ></Image>
         </div>
         <div className="inst-text flex flex-col  bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400  animate">
-          <p className="font-extrabold text-[24px] sm:text-3xl lg:text-[40px] lg:leading-normal">
+          <p className="font-extrabold max-sm:text-[16px] sm:text-3xl lg:text-[40px] lg:leading-normal">
             Sir M Visvesvaraya Institute of Technology
           </p>
 
-          <p className="text-[25px] font-semibold mt-3 mb-3">
+          <p className="lg:text-[25px] max-sm:text-[17px] font-semibold mt-3 mb-3">
             Bachelor of Engineering in Electronics and Communication Engineering
           </p>
-          <p className="text-[25px] font-semibold  mb-3">Aug 2019 - May 2023</p>
-          <p className="text-[25px] font-semibold  mb-3">CGPA - 8.8</p>
+          <p className="lg:text-[25px] max-sm:text-[17px] font-semibold  mb-3">Aug 2019 - May 2023</p>
+          <p className="lg:text-[25px] max-sm:text-[17px]  font-semibold  mb-3">CGPA - 8.8</p>
         </div>
       </div>
       <div className=" school flex max-sm:flex-col  px-[18px] gap-[50px] mt-5">
@@ -40,16 +40,16 @@ const Education = () => {
           ></Image>
         </div>
         <div className="inst-text flex flex-col  bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400  animate">
-          <p className="font-extrabold text-[24px] sm:text-3xl lg:text-[40px] lg:leading-normal">
+          <p className="font-extrabold max-sm:text-[16px] sm:text-3xl lg:text-[40px] lg:leading-normal">
             Kendriya Vidyalaya ASC Center South, Bengaluru
           </p>
-          <p className="text-[25px] font-semibold mt-3 mb-3">
+          <p className="lg:text-[25px] max-sm:text-[17px]  font-semibold mt-3 mb-3">
             Class XII (AISSCE)
           </p>
-          <p className="text-[25px] font-semibold  mb-3">
+          <p className="lg:text-[25px] max-sm:text-[17px]  font-semibold  mb-3">
             April 2018 - March 2019
           </p>
-          <p className="text-[25px] font-semibold  mb-3">Aggregate: 91.4%</p>
+          <p className="lg:text-[25px] max-sm:text-[17px] font-semibold  mb-3">Aggregate: 91.4%</p>
         </div>
       </div>
     </div>
